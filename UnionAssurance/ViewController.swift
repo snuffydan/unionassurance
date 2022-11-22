e76bb920b029c962304bda876c18c21cb8ca3c3d
Danushika Priyadarshani <danushikadan@gmail.com>
2022-11-22 19:19:05 +0530

    Initial Commit

---
+++ UnionAssurance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnionAssurance
+//
+//  Created by Danushika Priyadarshani on 2022-11-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
