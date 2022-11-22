d90102dca644506ac7752e0eaaf643b2b37060e2
Danushika Priyadarshani <danushikadan@gmail.com>
2022-11-22 23:35:05 +0530

    Added UIs

---
+++ UnionAssurance/Views/XIBs/RewardsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RewardsCollectionViewCell.swift
+//  UnionAssurance
+//
+//  Created by Danushika Priyadarshani on 2022-11-22.
+//
+
+import UIKit
+
+class RewardsCollectionViewCell: UICollectionViewCell {
+    
+}
